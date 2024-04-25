cd5aefca8795df122bcc465d74bb5b81ce15291e
Abhijith314 <147461848+Abhijith314@users.noreply.github.com>
2024-04-25 12:05:41 +0530

    Add files via upload

--- LFU_paging.c
+++ LFU_paging.c
@@ -1,59 +1,72 @@
 #include <stdio.h>
-
-int main() {
-    int i, j, k = 0, f, n, s = -1, m = 0, temp = 0, o;
-    printf("Enter the number of reference strings: ");
-    scanf("%d", &n);
-    printf("Enter the number of frames: ");
-    scanf("%d", &f);
-    int ref[n], p[f], counter[f];
-    printf("Enter the reference string: ");
-    for (i = 0; i < n; i++)
-        scanf("%d", &ref[i]);
-    for (i = 0; i < f; i++) {
-        p[i] = -1;
-        counter[i] = 0;
-    }
-    for (i = 0; i < n; i++) {
-        int a = ref[i];
-        int e = f - 1;
-        m = 0;
-        for (o = 0; o < f; o++) {
-            if (a == p[o]) {
-                temp++;
-                counter[o]++;
-            }
-        }
-        if (temp != 0) {
-            temp = 0;
-            m = 1;
-        }
-        printf("%d : ", ref[i]);
-        if (m == 1) {
-            for (j = 0; j < f; j++)
-                printf("-  ");
-            printf("\n");
-        } else {
-            int min_count = counter[0];
-            int min_index = 0;
-            for (j = 1; j < f; j++) {
-                if (counter[j] < min_count) {
-                    min_count = counter[j];
-                    min_index = j;
-                }
-            }
-            p[min_index] = a;
-            counter[min_index] = 1; // Reset the counter to 1 for LFU
-            for (j = 0; j < f; j++) {
-                if (p[j] == -1)
-                    printf("-  ");
-                else
-                    printf("%d  ", p[j]);
-            }
-            printf("\n");
-            k++;
-        }
-    }
-    printf("\nPage fault = %d\n", k);
-    return 0;
+struct frame
+{
+int content,freq,cnt;
+}frames[100];
+void main()
+{
+int i,j,pg,fr,cnt,pf,min,page[100],id=0;
+printf("Enter the number of reference string : ");
+scanf("%d",&pg);
+printf("Enter the number of frames : ");
+scanf("%d",&fr);
+printf("Enter the reference string : ");
+for(i=0;i<pg;i++) {
+scanf("%d",&page[i]);
+}
+for(i=0;i<fr;i++) {
+frames[i].content = -1;
+frames[i].freq = 0;
+frames[i].cnt = 0;
+}
+//printf("\nREFERENCING PAGE\tSTATUS\t\tFRAME CONTENT\n\n");
+for(pf=0,cnt=1,i=0;i<pg;i++)
+{
+printf("%d : ",page[i]);
+for(j=0;j<fr;j++)
+{
+if(frames[j].content == page[i])
+{
+//printf("HIT\t\t");
+frames[j].freq++;
+break;
+}
+}
+if(j == fr)
+{
+//printf("MISS\t\t");
+if(id<fr)
+{
+frames[id].content = page[i];
+frames[id].freq++;
+frames[id].cnt = cnt++;
+id++;
+}
+else
+{
+for(min=0,j=0;j<fr;j++)
+{
+if(frames[min].freq > frames[j].freq)
+{
+min = j;
+}
+else if(frames[min].freq == frames[j].freq&&frames[min].cnt>frames[j].cnt)
+{
+min = j;
+}
+}
+frames[min].content = page[i];
+frames[min].freq = 1;
+frames[min].cnt = cnt++;
+}
+pf++;
+}
+for(j=0;j<fr;j++)
+{
+if(frames[j].content !=-1)
+printf("%d  ",frames[j].content);
+}
+printf("\n");
+}
+printf("\nPage fault : %d\n",pf);
 }
