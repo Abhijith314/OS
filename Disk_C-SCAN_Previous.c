b899b40acd763b09bbd7987d8986a39c81ffb7db
Abhijith Pratheesh <147461848+Abhijith314@users.noreply.github.com>
2024-08-12 23:58:03 +0530

    Create Disk_C-SCAN_Previous.c

---
+++ Disk_C-SCAN_Previous.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<stdlib.h>
+int head,pre,n,i,j,a[50],move,temp,size,hd;
+void main()
+{
+  printf("Enter the current head position : ");
+  scanf("%d",&head);
+  printf("Enter the previous position : ");
+  scanf("%d",&pre);
+  size=200-1;
+  printf("Enter the number of request : ");
+  scanf("%d",&n);
+  printf("Enter the request : ");
+  for(i=0;i<n;i++) {
+    scanf("%d",&a[i]);
+  }
+  for(i=0;i<n;i++) {
+    for(j=0;j<n;j++) {
+      if(a[i]<a[j]){
+        temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
+      }
+    }
+  }
+   hd=head;
+    if (pre<head)
+    {
+        printf("The sequence is : %d ",head);
+        for(i=0;i<n;i++) {
+            if(a[i]>=hd) {
+                printf("-> %d",a[i]);
+                move+=abs(a[i]-head);
+                head=a[i];
+            }
+        }
+        printf(" -> %d",size);
+        move+=abs(size-head);
+        head=size;
+        printf(" -> 0");
+        move+=size;
+        head=0;
+        for(i=0;i<n;i++) {
+            if(a[i]<hd) {
+                printf("-> %d",a[i]);
+                move+=abs(a[i]-head);
+                head=a[i];
+            }
+        }
+        printf("\nTotal head movement = %d",move);
+    }
+    else
+    {
+        printf("The sequence is : %d",head);
+        for(i=n-1;i>=0;i--) {
+            if(a[i]<hd) {
+                printf(" -> %d",a[i]);
+                move+=abs(a[i]-head);
+                head=a[i];
+            }
+        }
+        printf(" -> 0");
+        move+=size;
+        head=0;
+        printf(" -> %d",size);
+        move+=abs(size-head);
+        head=size;
+        for(i=n-1;i>0;i--) {
+            if(a[i]>=hd) {
+                printf(" -> %d",a[i]);
+                move+=abs(a[i]-head);
+                head=a[i];
+            }
+        }
+        printf("\nTotal head movement = %d",move);
+    }
+}
